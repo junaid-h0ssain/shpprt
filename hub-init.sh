edab2a6c72b4535f5f9d7203102e32d6de4eb03c
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-05-26 05:18:18 +0600

    init commit with separate backend & frontend

---
+++ hub-init.sh
@@ -0,0 +1,5 @@
+git remote add cb ssh://git@codeberg.org/juna1d/shpprt.git
+git push -u cb main
+
+git remote add gh git@github.com:junaid-h0ssain/shpprt.git
+git push -u gh main
---
+++ hub.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: ./hub.sh 'commit message'"
+  exit 1
+fi
+
+git add .
+git commit -m "$1"
+git push cb && git push gh
