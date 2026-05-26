d73e233273be94f82b29c2ebb65143c199fa02ea
junaid-hossain <158271661+junaid-h0ssain@users.noreply.github.com>
2026-05-26 06:07:44 +0600

    env setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bunny");
+import express  from "express";
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors";
+
+const app = express();
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: ENV.FRONTEND_URL,
+  credentials: true
+}));
+
+app.get("/", (req, res) => {
+  res.json({"Hello World!" : "Welcome to the backend server"});
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on port " + ENV.PORT);
+});
